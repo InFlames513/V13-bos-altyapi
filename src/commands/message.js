3de0f38940336e07c8bda5401efa6b93f4b84807
InFlames <85372327+InFlames513@users.noreply.github.com>
2022-05-14 22:10:08 +0300

    Update message.js

--- src/commands/message.js
+++ src/commands/message.js
@@ -2,7 +2,7 @@
 
 module.exports = {
     slash: false,
-    name: ['test', 'deneme'], //arraya istediğiniz kadar kullanım yazabilirsiniz alieses gibi saçma bir şeyle uğraşmak yerine direk arraya ekleyebilirsiniz.
+    name: ['test', 'deneme'], //arraya istediğiniz kadar kullanım yazabilirsiniz aliases gibi saçma bir şeyle uğraşmak yerine direk arraya ekleyebilirsiniz.
     async execute(client, message, args) {
         return message.channel.send(client.user.username + args[1]);
     }
