1599a7cabd5a48684ce4bcb40e1f7001139acb26
InFlames <85372327+InFlames513@users.noreply.github.com>
2022-05-15 03:14:51 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//botun main dosyası 
+
 const discord = require("discord.js");
 const client = new discord.Client({ intents: Object.values(discord.Intents.FLAGS).reduce((x, y) => x + y, 0) });
 const { token } = require("./src/base/settings.json");
