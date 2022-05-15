8abe3f504cb9cacb4e6712ada8f01f1cd3a68451
InFlames <85372327+InFlames513@users.noreply.github.com>
2022-05-15 03:15:29 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,5 @@
+//test botu için main dosya
+
 const discord = require("discord.js");
 const client = new discord.Client({ intents: Object.values(discord.Intents.FLAGS).reduce((x, y) => x + y, 0) });
 const { testToken } = require("./src/base/settings.json");
