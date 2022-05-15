67ddc142ad8c30d60f913ceff791992e7565acdb
InFlames <85372327+InFlames513@users.noreply.github.com>
2022-05-15 03:12:45 +0300

    Update interactionCreate.js

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -1,14 +1,19 @@
+//burasının ne olduğunu bilmiyorsanız lütfen hiç bir koda dokunmayın.
+
 module.exports = {
 	name: 'interactionCreate',
 	execute(interaction, client) {
-	  if (!interaction.isCommand()) return;
-      const command = client.slashcommands.get(interaction.commandName);
-	  if (!command) return;
-      try {
-        command.execute(client, interaction);
-	  } catch (error) {
-		console.error(error);
-        interaction.reply({ content: 'Komutta bir sorun oluştu lütfen daha sonra tekrar dene 😔', ephemeral: true });
+	  if (!interaction.isCommand()) {
+		  //butona basıldığında burda hangi işlemleri yapacağınızı belirleyebilirsiniz.
+	  } else {
+		const command = client.slashcommands.get(interaction.commandName);
+		if (!command) return;
+		try {
+		  command.execute(client, interaction);
+		} catch (error) {
+		  console.error(error);
+		  interaction.reply({ content: 'Komutta bir sorun oluştu lütfen daha sonra tekrar dene 😔', ephemeral: true });
+		}
 	  }
 	},
 };
